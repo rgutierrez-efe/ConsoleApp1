a570ae624555e0dd18389d5fd14b03db96d3457f
Ruben <Ruben@PT-TEC-RGUTI>
2024-09-05 12:37:47 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("hola");
+                System.Threading.Thread.Sleep(1000*5);
+            }
+        }
+    }
+}
